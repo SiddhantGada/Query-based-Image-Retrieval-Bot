03d8e5cc60a5e95efa425554f772412cf1ae3190
Siddhant Gada <siddhantgada@sids-mbp.home>
2018-11-26 21:31:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# cloud_hw3
+-Cloud Computing and Big Data HW3
+-Team members: Prerna Kashyap (UNI: pk2600), Siddhant Gada (UNI: scg2151)
+-To run code do:
+```
+npm install
+npm install aws-api-gateway-client
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import './hello.js';
+
+class App extends Component {
+  render() {
+    return (
+      <body>
+      <div>
+      <div id='bodybox'>
+    <div id='chatborder'>
+      <p id="chatlog7" class="chatlog">&nbsp;</p>
+      <p id="chatlog6" class="chatlog">&nbsp;</p>
+      <p id="chatlog5" class="chatlog">&nbsp;</p>
+      <p id="chatlog4" class="chatlog">&nbsp;</p>
+      <p id="chatlog3" class="chatlog">&nbsp;</p>
+      <p id="chatlog2" class="chatlog">&nbsp;</p>
+      <p id="chatlog1" class="chatlog">&nbsp;</p>
+      <input type="text" name="chat" id="chatbox" placeholder="Hi there! Type here to talk to me." onfocus="placeHolder()"
+      />
+      <input type="file" onChange={this.fileChangedHandler} class='upload_1'/>
+        <button onClick={this.uploadHandler} class='upload_1'>Upload!</button>
+    </div>
+  </div>
+</div>
+</body>
+    );
+  }
+}
+
+
+
+export default App;
